8b78bfadebd4dc13dd4d8be1fb999d7d750b0fc2
JMBenitez-dev <105891765+JMBenitez-dev@users.noreply.github.com>
2022-06-04 12:23:08 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,30 @@ let cantidadButacasOcupadas : number = 0;
 function cargarVector(vCargar : boolean[], cantidadCargar : number) {
   for (let indice:number = 0; indice<cantidadCargar; indice++) {
   vCargar[indice] =  Boolean(Math.floor(Math.random() * (2 - 0) + 0));
+    //si en vez de 2 y los ceros ponias variables como max y min quedaria mejor, sino directamente vCargar[indice] =  Boolean(Math.floor(Math.random() * 2))
   };
 };
 
 function mostrarVector(vMostrar : boolean[], cantidadMostrar : number) {
   for (let indice:number = 0; indice<cantidadMostrar; indice++) {
   console.log(vMostrar[indice] + "  ");
+    //esto no está mal pero te va a mostrar una lista interminable, si el ejercicio no te lo pide hacer console.log(vector) y listo.
   };
 };
 
 function informarButacas(vInformar : boolean[], cantidadInformar : number){
   for (let indice : number = 0;indice < cantidadInformar; indice++){
     if (vInformar[indice] === true){
-      cantidadButacasOcupadas++
+      cantidadButacasOcupadas++;
     } else if (vInformar[indice] === false){
-      cantidadButacasLibres++
+      cantidadButacasLibres++;
     }
-  };
-  console.log("La cantidad de Butacas libres es: " + cantidadButacasLibres)
-  console.log("La cantidad de Butacas ocupadas es: " + cantidadButacasOcupadas)
+  }
+  console.log("La cantidad de Butacas libres es: " + cantidadButacasLibres);
+  console.log("La cantidad de Butacas ocupadas es: " + cantidadButacasOcupadas);
 };
 
 cargarVector(arregloDeButacas,cantidadButacas);
 mostrarVector(arregloDeButacas,cantidadButacas);
-informarButacas(arregloDeButacas,cantidadButacas)
+informarButacas(arregloDeButacas,cantidadButacas);
+//Esta bien tu solucion
